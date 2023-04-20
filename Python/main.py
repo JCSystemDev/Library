77d1b5a53a0411445e45d8dd1ae87895e91edef4
JC System <juancarlosgallardo87@gmail.com>
2023-04-19 21:16:47 -0400

    Conexión a Mongo

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,21 @@
 # Menú Principal de la aplicación
+
+import pymongo
+import tkinter
+
+mongoHost = "localhost"
+mongoPort = "27017"
+mongoTimeout = 1000
+mongoUrl = "mongodb://"+mongoHost+":"+mongoPort+"/"
+
+try:
+    client = pymongo.MongoClient(mongoUrl, serverSelectionTimeoutMS = mongoTimeout)
+    client.server_info()
+    print("Conexión a Mongo exitosa")
+    client.close()
+except pymongo.errors.ServerSelectionTimeoutMS as errorTime:
+    print("Tiempo Excedido "+errorTime)
+except pymongo.errors.ConnectionFailure as errorConnection:
+    print("Fallo al conectarse a MongoDB"+errorConnection)
+
+

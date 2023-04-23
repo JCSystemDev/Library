e56b01dc20c8562f9efdf9198a2d38c2e3f253b9
JC System <juancarlosgallardo87@gmail.com>
2023-04-22 22:13:33 -0400

    CRUD Terminado

--- Python/main.py
+++ Python/main.py
@@ -16,15 +16,20 @@ dataBase = client[mongo_db]
 collection = dataBase[mongo_collection]
 id_user = ""
 
-def displayDocuments():
+def displayDocuments(usuario="",correo=""):
+    objectSearch={}
+    if len(usuario)!=0:
+        objectSearch["user"]=usuario
+    if len(correo)!=0:
+        objectSearch["email"]=correo
     try:
         rows = table.get_children()
         for row in rows:
             table.delete(row)
-        for document in collection.find():
+        for document in collection.find(objectSearch):
             table.insert('', 0,text=document["_id"],values=document["user"])
     except pymongo.errors.ServerSelectionTimeoutMS as errorTime:
-        print("Tiempo Excedido "+errorTime)
+        print("Tiempo Excedido"+errorTime)
     except pymongo.errors.ConnectionFailure as errorConnection:
         print("Fallo al conectarse a MongoDB"+errorConnection)
 
@@ -43,23 +48,6 @@ def insertData():
     else:
         messagebox.showerror(message="Los campos no pueden estar vacíos")
     displayDocuments()
-
-def doubleClickTable(event):
-    global id_user
-    id_user = str(table.item(table.selection())["text"])
-    document = collection.find({"_id":ObjectId(id_user)})[0]
-    usuario.delete(0,END)
-    usuario.insert(0, document["user"])
-    clave.delete(0,END)
-    clave.insert(0, document["password"])
-    nombre.delete(0,END)
-    nombre.insert(0, document["name"])
-    apellido.delete(0,END)
-    apellido.insert(0, document["lastname"])
-    correo.delete(0,END)
-    correo.insert(0, document["email"])
-    insert["state"]="disabled"
-    update["state"]="normal"
 
 def updateData():
     global id_user
@@ -81,6 +69,46 @@ def updateData():
     displayDocuments()        
     insert["state"]="normal"
     update["state"]="disabled"
+    delete["state"]="disabled"
+
+def deleteData():
+    global id_user
+    try:
+        idSearch={"_id":ObjectId(id_user)}
+        collection.delete_one(idSearch)
+        usuario.delete(0,END)
+        clave.delete(0,END)
+        nombre.delete(0,END)
+        apellido.delete(0,END)
+        correo.delete(0,END)     
+    except pymongo.errors.ConnectionFailure as error:
+        print(error)
+    insert["state"]="normal"
+    update["state"]="disabled"
+    delete["state"]="disabled"
+    displayDocuments()
+
+def searchData():
+    displayDocuments(buscarUsuario.get(), buscarCorreo.get())
+
+def doubleClickTable(event):
+    global id_user
+    id_user = str(table.item(table.selection())["text"])
+    document = collection.find({"_id":ObjectId(id_user)})[0]
+    usuario.delete(0,END)
+    usuario.insert(0, document["user"])
+    clave.delete(0,END)
+    clave.insert(0, document["password"])
+    nombre.delete(0,END)
+    nombre.insert(0, document["name"])
+    apellido.delete(0,END)
+    apellido.insert(0, document["lastname"])
+    correo.delete(0,END)
+    correo.insert(0, document["email"])
+    insert["state"]="disabled"
+    update["state"]="normal"
+    delete["state"]="normal"
+
 
 
 #Interfaz gráfica
@@ -128,20 +156,48 @@ correo.grid(row=8,column=1)
 Label(window, bg="black", height=1).grid(row=9)
 
 #Botón Insertar
-insert = Button(window, text="Insertar Datos", command=insertData, bg="black", fg="white")
+insert = Button(window, text="Insertar Usuario", command=insertData, bg="black", fg="white")
 insert.grid(row=10, columnspan=2)
 
 #Espacio
 Label(window, bg="black", height=1).grid(row=11)
 
 #Botón Actualizar
-update = Button(window, text="Actualizar Datos", command=updateData, bg="black", fg="white")
+update = Button(window, text="Actualizar Usuario", command=updateData, bg="black", fg="white")
 update.grid(row=12, columnspan=2)
 update["state"]="disabled"
 
-
 #Espacio
 Label(window, bg="black", height=1).grid(row=13)
+
+#Botón Borrar
+delete = Button(window, text="Borrar Usuario", command=deleteData, bg="black", fg="white")
+delete.grid(row=14, columnspan=2)
+delete["state"]="disabled"
+
+#Espacio
+Label(window, bg="black", height=1).grid(row=15)
+
+#Buscar por Usuario
+Label(window, text="Buscar Usuario", bg="black", fg="white").grid(row=16,column=0)
+buscarUsuario = Entry(window)
+buscarUsuario.grid(row=16,column=1)
+
+#Correo
+Label(window, text="Buscar E-mail", bg="black", fg="white").grid(row=17,column=0)
+buscarCorreo = Entry(window)
+buscarCorreo.grid(row=17,column=1)
+
+#Espacio
+Label(window, bg="black", height=1).grid(row=18)
+
+#Botón Buscar
+search = Button(window, text="Buscar", command=searchData, bg="black", fg="white")
+search.grid(row=19, columnspan=2)
+search["state"]="normal"
+
+#Espacio
+Label(window, bg="black", height=1).grid(row=20)
 
 displayDocuments()
 window.mainloop()
